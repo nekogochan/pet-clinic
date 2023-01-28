0195305426bdf54bcb88be2cf2ae9afed2afc0ab
Silantev Georgiy <silantev@aointell.ru>
2023-01-29 00:22:26 +0300

    fix CrudController

--- src/main/java/nekogochan/petclinic/core/crud/controller/CrudController.java
+++ src/main/java/nekogochan/petclinic/core/crud/controller/CrudController.java
@@ -1,12 +1,13 @@
 package nekogochan.petclinic.core.crud.controller;
 
-import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import nekogochan.petclinic.core.crud.beanpostprocessor.CrudDependsOn;
 import nekogochan.petclinic.core.crud.service.CrudService;
 import nekogochan.petclinic.core.entity.BaseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -31,7 +32,7 @@ public abstract class CrudController<E extends BaseEntity<ID>, ID> implements Ha
     @CrudDependsOn(CrudService.WithSave.class)
     public interface WithSave<E extends BaseEntity<ID>, ID> extends HasCrudService<E, ID> {
         @PostMapping("/save")
-        default ID save(E entity) {
+        default ID save(@RequestBody E entity) {
             return ((CrudService.WithSave<E, ID>) crudService()).save(entity).getId();
         }
     }
@@ -39,7 +40,7 @@ public abstract class CrudController<E extends BaseEntity<ID>, ID> implements Ha
     @CrudDependsOn(CrudService.WithGetOne.class)
     public interface WithGetOne<E extends BaseEntity<ID>, ID> extends HasCrudService<E, ID> {
         @GetMapping("/{id}")
-        default E getOne(@PathParam("id") ID id) {
+        default E getOne(@PathVariable("id") ID id) {
             return ((CrudService.WithGetOne<E, ID>) crudService()).getOne(id)
                     .orElseThrow();
         }
